febbaf69b348cf9622bf0324ed5245bb66a981f1
Piotr Poterek <peterpoterek0@gmail.com>
2023-07-27 03:32:34 +0200

    Number buttons switch statement

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,63 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [currentNum, setCurrentNum] = useState(0);
+  const handleNumClick = (num) => {
+    switch (num) {
+      case 1:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      case 2:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      case 3:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      case 4:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      case 5:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      case 6:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      case 7:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      case 8:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      case 9:
+        console.log(num);
+        setCurrentNum(num);
+        break;
+      default:
+        console.log("Invalid number");
+    }
+  };
   return (
     <>
       <h1>012321</h1>
       <div className="wrapper">
         <div className="container-num">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+          <button onClick={() => handleNumClick(1)}>1</button>
+          <button onClick={() => handleNumClick(2)}>2</button>
+          <button onClick={() => handleNumClick(3)}>3</button>
+          <button onClick={() => handleNumClick(4)}>4</button>
+          <button onClick={() => handleNumClick(5)}>5</button>
+          <button onClick={() => handleNumClick(6)}>6</button>
+          <button onClick={() => handleNumClick(7)}>7</button>
+          <button onClick={() => handleNumClick(8)}>8</button>
+          <button onClick={() => handleNumClick(9)}>9</button>
         </div>
         <div className="container-operator">
           <button>+</button>
