861d7ec6d18194781d8d3647ffb588526e8f2768
Piotr Poterek <peterpoterek0@gmail.com>
2023-07-27 03:12:24 +0200

    ui setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,27 @@ function App() {
 
   return (
     <>
-      <h1>Test</h1>
+      <h1>012321</h1>
+      <div className="wrapper">
+        <div className="container-num">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
+        <div className="container-operator">
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+        </div>
+      </div>
+      <button>=</button>
     </>
   );
 }
