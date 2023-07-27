97d563df43ffb5318019cd5be44e1bf4964cda34
Piotr Poterek <peterpoterek0@gmail.com>
2023-07-27 17:13:42 +0200

    fixed bug when switching operators

--- src/Components/Operators.jsx
+++ src/Components/Operators.jsx
@@ -2,10 +2,13 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 const Operators = () => {
-  const { currentNum, setPrevNumber, currentOperator, setCurrentOperator, clearUI } = useContext(AppContext);
+  const { currentNum, prevNumber, setPrevNumber, currentOperator, setCurrentOperator, clearUI } = useContext(AppContext);
   const handleOperator = (operator) => {
+    if (currentOperator === operator) return;
+    if (currentNum === "") return;
     setCurrentOperator(operator);
     setPrevNumber(currentNum);
+
     clearUI(true, false, false, false);
 
     console.log(currentOperator);
