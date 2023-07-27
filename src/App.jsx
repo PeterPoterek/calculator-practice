40ac3570c842e47b1a0baa5852fdb54af1ab59ad
Piotr Poterek <peterpoterek0@gmail.com>
2023-07-27 16:35:48 +0200

    code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
 import { useState, createContext } from "react";
-
 import "./App.css";
 import NumberButtons from "./Components/NumberButtons";
 import Operators from "./Components/Operators";
 
 export const AppContext = createContext();
+
 function App() {
   const [currentNum, setCurrentNum] = useState("");
   const [currentOperator, setCurrentOperator] = useState("");
-
   const [prevNumber, setPrevNumber] = useState("");
-
   const [result, setResult] = useState("");
 
   const clearUI = (currNumFlag, prevNumFlag, resultFlag, operatorFlag) => {
-    if (currNumFlag) {
-      setCurrentNum("");
-    }
-    if (prevNumFlag) {
-      setPrevNumber("");
-    }
-    if (resultFlag) {
-      setResult("");
-    }
-    if (operatorFlag) {
-      setCurrentOperator("");
-    }
+    if (currNumFlag) setCurrentNum("");
+    if (prevNumFlag) setPrevNumber("");
+    if (resultFlag) setResult("");
+    if (operatorFlag) setCurrentOperator("");
   };
 
   const clear = () => {
@@ -36,27 +26,42 @@ function App() {
     let num1 = Number(currentNum);
     let num2 = Number(prevNumber);
     let result = 0;
-    if (currentOperator === "+") {
-      result = num1 + num2;
-    }
-    if (currentOperator === "-") {
-      result = num1 - num2;
-    }
-    if (currentOperator === "*") {
-      result = num1 * num2;
-    }
-    if (currentOperator === "/") {
-      result = num2 / num1;
+
+    switch (currentOperator) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num2 / num1;
+        break;
+      default:
+        return;
     }
 
     clearUI(false, true, false, true);
     setResult(result);
     setCurrentNum(result);
-    console.log(result);
   };
+
   return (
     <>
-      <AppContext.Provider value={{ currentNum, setCurrentNum, prevNumber, setPrevNumber, currentOperator, setCurrentOperator, clearUI }}>
+      <AppContext.Provider
+        value={{
+          currentNum,
+          setCurrentNum,
+          prevNumber,
+          setPrevNumber,
+          currentOperator,
+          setCurrentOperator,
+          clearUI,
+        }}
+      >
         <span>{prevNumber}</span>
         <span>{currentOperator}</span>
         <span>{currentNum} </span>
